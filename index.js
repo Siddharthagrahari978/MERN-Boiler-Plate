bd47de3891f1a5603af03dedec0d97140fc71745
siddharthagrahari978 <siddharthagrahari978@gmail.com>
2020-08-09 20:02:46 +0530

    Update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+//! Mongoose connecting to MongoDB Database
 mongoose
   .connect(
     "mongodb+srv://node-app:node-app978@node-applications-data.4s7nb.mongodb.net/<dbname>?retryWrites=true&w=majority",
@@ -9,10 +13,17 @@ mongoose
   .then(() => console.log("DB Connected..."))
   .catch((err) => console.error(err));
 
+//! Sending result for requesting with "/"
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+//!
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//! Deploy App on localhost
 const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () =>
   console.log(`App deployed successfully at http://localhost:${PORT}`)
